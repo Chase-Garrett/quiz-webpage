ef8cef2b4b275e2daf7ca4968dc72e71096dda56
Chase <chasegarrett@tutanota.com>
2023-07-12 19:00:45 -0500

    added timer and started debugging start quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#start");
+var timer = document.querySelector("#time");
+
+var secondsLeft = 75;
+
+// Timer function
+function setTime() {
+    timer.textContent = "Time: " + secondsLeft;
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = "Time: " + secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            timer.textContent = "Time: 0";
+        }
+    }, 1000);
+}
 
 // startQuiz function
 function startQuiz() {
+    // displays timer
+    setTime();
     // removes text from quiz-body div
-    var quizBody = document.getElementById("quiz-body");
+    var quizBody = document.getElementsByClassName("quiz-body")[0];
     quizBody.textContent = "";
 
-    // change timer to 75 seconds and display it
-    var timer = document.getElementById("time");
-    timer.textContent = "Time: 75";
-
-
     // changes text of quiz-header div to first question
-    var quizHeader = document.getElementById("quiz-header");
+    var quizHeader = document.getElementsByClassName("quiz-header")[0];
     quizHeader.textContent = "Commonly used data types DO NOT include:";
 
     // creates 4 buttons in the same style as the start button and appends them to the quiz-footer div
@@ -21,8 +35,7 @@ function startQuiz() {
     // each button has an answer choice as its text content
     // each button has an event listener that calls the checkAnswer function
     // removes start button
-    var quizFooter = document.getElementById("quiz-footer");
-    quizFooter.removeAttribute(generateBtn);
+    var quizFooter = document.getElementsByClassName("quiz-footer")[0];
     var button1 = document.createElement("button");
     button1.setAttribute("class", "btn btn-primary btn-lg btn-block");
     button1.setAttribute("id", "button1");
